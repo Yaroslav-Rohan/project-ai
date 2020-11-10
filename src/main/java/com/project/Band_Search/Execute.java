7760eaf0126ac222d014d75922aea3d9a1eaf92e
O_D_I_U_M <desolh1z1@gmail.com>
2020-11-11 01:56:44 +0200

    Spring setup

---
+++ src/main/java/com/project/Band_Search/Execute.java
@@ -0,0 +1,11 @@
+package com.project.Band_Search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Execute {
+    public static void main(String[] args) {
+        SpringApplication.run(Execute.class, args);
+    }
+}
