35a05fe369dd3b2094bf9d48989529d5b0e72156
O_D_I_U_M <desolh1z1@gmail.com>
2020-12-09 13:53:52 +0200

    no new

--- src/main/java/com/project/Band_Search/repository/UserRepository.java
+++ src/main/java/com/project/Band_Search/repository/UserRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     User findByEmail(String email);
   //  User findByResetToken(String token);
     User findByResetPasswordToken(String resetToken);
